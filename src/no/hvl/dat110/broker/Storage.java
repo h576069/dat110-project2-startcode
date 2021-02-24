8a7eb52e3afc21d597fcfcc31511ad87bc619ec9
AnneDrottning <AnneDrottning@158-37-232-44.hvl.no>
2021-02-24 13:56:50 +0100

    Task B.1 --felles på zoom

--- src/no/hvl/dat110/broker/Storage.java
+++ src/no/hvl/dat110/broker/Storage.java
@@ -1,11 +1,10 @@
 package no.hvl.dat110.broker;
 
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
-import no.hvl.dat110.common.TODO;
-import no.hvl.dat110.common.Logger;
 import no.hvl.dat110.messagetransport.Connection;
 
 public class Storage {
@@ -13,10 +12,10 @@ public class Storage {
 	// data structure for managing subscriptions
 	// maps from a topic to set of subscribed users
 	protected ConcurrentHashMap<String, Set<String>> subscriptions;
-	
+
 	// data structure for managing currently connected clients
 	// maps from user to corresponding client session object
-	
+
 	protected ConcurrentHashMap<String, ClientSession> clients;
 
 	public Storage() {
@@ -36,7 +35,7 @@ public class Storage {
 
 	// get the session object for a given user
 	// session object can be used to send a message to the user
-	
+
 	public ClientSession getSession(String user) {
 
 		ClientSession session = clients.get(user);
@@ -51,49 +50,39 @@ public class Storage {
 	}
 
 	public void addClientSession(String user, Connection connection) {
-
 		// TODO: add corresponding client session to the storage
-		
-		throw new UnsupportedOperationException(TODO.method());
-		
+		clients.put(user, new ClientSession(user, connection));
+
 	}
 
 	public void removeClientSession(String user) {
-
 		// TODO: remove client session for user from the storage
+		clients.remove(user);
+		// Fjerne på subscriptions og?
 
-		throw new UnsupportedOperationException(TODO.method());
-		
 	}
 
 	public void createTopic(String topic) {
-
 		// TODO: create topic in the storage
+		subscriptions.put(topic, new HashSet<String>());
 
-		throw new UnsupportedOperationException(TODO.method());
-	
 	}
 
 	public void deleteTopic(String topic) {
-
 		// TODO: delete topic from the storage
+		subscriptions.remove(topic);
 
-		throw new UnsupportedOperationException(TODO.method());
-		
 	}
 
 	public void addSubscriber(String user, String topic) {
-
 		// TODO: add the user as subscriber to the topic
-		
-		throw new UnsupportedOperationException(TODO.method());
-		
+		subscriptions.get(topic).add(user);
+
 	}
 
 	public void removeSubscriber(String user, String topic) {
-
 		// TODO: remove the user as subscriber to the topic
+		subscriptions.get(topic).remove(user);
 
-		throw new UnsupportedOperationException(TODO.method());
 	}
 }
--- src/no/hvl/dat110/messages/CreateTopicMsg.java
+++ src/no/hvl/dat110/messages/CreateTopicMsg.java
@@ -1,28 +1,31 @@
 package no.hvl.dat110.messages;
 
 public class CreateTopicMsg extends Message {
-private String topic;
+	private String topic;
 
-public CreateTopicMsg(String user, String topic){
-super(MessageType.CREATETOPIC, user);
-this.topic=topic;
-	// message sent from client to create topic on the broker
-	
-	// TODO: 
-	// Implement object variables - a topic is required
-	
-	// Constructor, get/set-methods, and toString method
-    // as described in the project text	
-}
-public String getTopic() {
-	return topic;
-}
-public void setTopic(String topic) {
-	this.topic = topic;
-}
-@Override
-public String toString() {
-	return "CreateTopicMsg [topic=" + topic + "]";
-}
+	public CreateTopicMsg(String user, String topic) {
+		super(MessageType.CREATETOPIC, user);
+		this.topic = topic;
+		// message sent from client to create topic on the broker
+
+		// TODO:
+		// Implement object variables - a topic is required
+
+		// Constructor, get/set-methods, and toString method
+		// as described in the project text
+	}
+
+	public String getTopic() {
+		return topic;
+	}
+
+	public void setTopic(String topic) {
+		this.topic = topic;
+	}
+
+	@Override
+	public String toString() {
+		return "CreateTopicMsg [topic=" + topic + "]";
+	}
 
 }
