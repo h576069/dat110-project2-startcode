e83fe3d748004e145fc9c6aba06762827f59edaf
AnneDrottning <AnneDrottning@158-37-232-44.hvl.no>
2021-02-24 14:37:20 +0100

    Forsøk 1 Task C --alle på zoom

--- src/no/hvl/dat110/iotsystem/DisplayDevice.java
+++ src/no/hvl/dat110/iotsystem/DisplayDevice.java
@@ -6,29 +6,39 @@ import no.hvl.dat110.messages.PublishMsg;
 import no.hvl.dat110.common.TODO;
 
 public class DisplayDevice {
-	
+
 	private static final int COUNT = 10;
-		
-	public static void main (String[] args) {
-		
+
+	public static void main(String[] args) {
+
 		System.out.println("Display starting ...");
-		
+
 		// TODO - START
-				
+
 		// create a client object and use it to
-		
+		Client c = new Client("disp", Common.BROKERHOST, Common.BROKERPORT);
+
 		// - connect to the broker
+		c.connect();
 		// - create the temperature topic on the broker
+		c.createTopic(Common.TEMPTOPIC);
 		// - subscribe to the topic
+		c.subscribe(Common.TEMPTOPIC);
 		// - receive messages on the topic
-		// - unsubscribe from the topic
-		// - disconnect from the broker
-		
-		// TODO - END
+		for (int i = 0; i < COUNT; i++) {
+			Message msg = c.receive();
+			System.out.println(msg);
+		}
 		
 		System.out.println("Display stopping ... ");
-		
-		throw new UnsupportedOperationException(TODO.method());
-		
+
+		// - unsubscribe from the topic
+		c.unsubscribe(Common.TEMPTOPIC);
+		// - disconnect from the broker
+		c.disconnect();
+
+		// TODO - END
+
+
 	}
 }
--- src/no/hvl/dat110/iotsystem/TemperatureDevice.java
+++ src/no/hvl/dat110/iotsystem/TemperatureDevice.java
@@ -15,16 +15,26 @@ public class TemperatureDevice {
 		// TODO - start
 
 		// create a client object and use it to
-
+		//String user, String server, int port
+		Client c = new Client("temp",Common.BROKERHOST,Common.BROKERPORT);
+		
 		// - connect to the broker
+		c.connect();
+		
 		// - publish the temperature(s)
+		for (int i = 0; i < COUNT; i++) {
+			int temp = sn.read();
+			c.publish(Common.TEMPTOPIC, ""+temp);
+		}
+		
+		
 		// - disconnect from the broker
-
+		c.disconnect();
 		// TODO - end
 
 		System.out.println("Temperature device stopping ... ");
 
-		throw new UnsupportedOperationException(TODO.method());
+//		throw new UnsupportedOperationException(TODO.method());
 
 	}
 }
